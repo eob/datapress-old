104f5d88dd90eda9d1b3f3edd40491e77fe0b303
Edward Benson <edward.benson@gmail.com>
2011-10-26 16:55:54 -0400

    adding and renaming editor files

---
+++ plugin-scaffold/data-editor.php
@@ -0,0 +1,27 @@
+<?
+class DatapressDataEditor {
+ 
+  function Bootup() {
+    // Add the exhibit button 
+    add_action('media_buttons', array($this, 'InsertEditorButtons'));
+    add_action('wp_ajax_datapress_editor', array($this, 'OpenInClient'));
+  }
+
+  function InsertEditorButtons() {
+    $editorLink = wp_guess_url() . "/wp-admin/admin-ajax.php?action=datapress_edtor";
+    $imgLink = wp_guess_url() . "/wp-content/plugins/datapress/images/editor-button.png";
+    $button = <<<HTML
+Visualization 
+<a id="loadDatapressjditor" href="$editorLink" class="thickbox" title="Add an Exhibit"><img src="$imgLink" /></a> 
+HTML;
+    echo $button;
+  }
+
+  function OpenInClient() {
+  }
+
+  function SaveConfiguration() {
+  }
+
+}
+?>
---
+++ plugin-scaffold/viz-editor.php
@@ -0,0 +1,33 @@
+<?
+class DatapressVizEditor {
+ 
+  function Bootup() {
+    // Add the exhibit button 
+    add_action("admin_init", array($this, 'RegisterMetaBoxes'));
+  }
+ 
+  function RegisterMetaBoxes() {
+    foreach (array('post','page') as $type) {
+      add_meta_box('datapress_data_editor',
+                   'Add Data',
+                   array($this, 'DisplayEditor'),
+                   $type,
+                   'normal',
+                   'high');
+    }
+    add_action('save_post', array($this, 'SaveData'));
+  }
+
+  function DisplayEditor() {
+    $editorHtml = <<<HTML
+##INCLUDE:../viz-editor/editor.html:EDITOR
+HTML;
+   echo $editorHtml; 
+  }
+
+  function SaveData() {
+   // The user is saving a post. So save the data from the data editor here.
+  }
+
+}
+?>
